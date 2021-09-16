0fbb6163fdeb2c9ddeddc2164ce4d3607c9d266b
PEDRO AVENA <pedroavena55@gmail.com>
2021-09-16 15:47:43 -0300

    simple form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form'
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <label htmlFor="title">Title:</label>
+        <input id='title' type="text" {...register('title')} />
+        <label htmlFor="body">Body:</label>
+        <input id='body' type="text" {...register('body')} />
+        <label htmlFor="user-id">User ID:</label>
+        <input id='user-id' type="number" {...register('userId', { valueAsNumber: true })} />
+        <button type='submit'>Enviar</button>
+
+
+      </form>
     </div>
   );
 }
